70f895538932985bf623faaf0cee2c105431c9af
ViswaMallavarapu <viswaembeddedtech@gmail.com>
2025-04-05 13:15:40 +0530

    size of structure

---
+++ sizestruct.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+struct student {
+	int rollnum;
+	int class;
+	int phnnum;
+};
+int main()
+{
+	struct student Jyothi;
+       printf("Size of structure : %lu\n",sizeof(Jyothi));
+
+}
